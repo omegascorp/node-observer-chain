dfa3854740cc18d7ac386173230fb3ed04cb5b61
omegascorp <omegascorp@gmail.com>
2015-09-21 23:38:27 +0400

    Use es6 classes

--- index.js
+++ index.js
@@ -1,30 +1,33 @@
-function Observable () {
-    this.observers = [];
-}
-Observable.prototype.addObserver = function(observer) {
-    this.observers.push(observer);
-};
-Observable.prototype.removeObserver = function(observer) {
-    var that = this;
-    this.observers.forEach(function(currentObserver, index) {
-        if (currentObserver === observer) {
-            that.observers = that.observers.splice(index, 1);
-            return false;
-        }
-    });
-};
-Observable.prototype.notifyObservers = function() {
-    var args = arguments;
-    this.observers.forEach(function(observer) {
-        observer.handleEvent.apply(observer, args);
-    });
-};
+'use strict';
 
-function Observer () {
-
+class Observable {
+    constructor() {
+        this.observers = [];
+    }
+    addObserver(observer) {
+        this.observers.push(observer);
+    }
+    removeObserver(observer) {
+        var that = this;
+        this.observers.forEach(function(currentObserver, index) {
+            if (currentObserver === observer) {
+                that.observers = that.observers.splice(index, 1);
+                return false;
+            }
+        });
+    }
+    notifyObservers() {
+        var args = arguments;
+        this.observers.forEach(function(observer) {
+            observer.handleEvent.apply(observer, args);
+        });
+    }
 }
-Observer.prototype.handleEvent = function() {
-};
+
+class Observer {
+    handleEvent() {
+    }
+}
 
 module.exports.Observable = Observable;
 module.exports.Observer = Observer;
