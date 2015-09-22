881803a220917158f0987e5da459130f42fa4636
omegascorp <omegascorp@gmail.com>
2015-09-22 22:42:36 +0400

    Change model of saving observers from array to chain. Add ObservableSet object.

--- index.js
+++ index.js
@@ -1,32 +1,5 @@
 'use strict';
 
-class Observable {
-    constructor() {
-        this.observers = [];
-    }
-    addObserver(observer) {
-        this.observers.push(observer);
-    }
-    removeObserver(observer) {
-        var that = this;
-        this.observers.forEach(function(currentObserver, index) {
-            if (currentObserver === observer) {
-                that.observers = that.observers.splice(index, 1);
-                return false;
-            }
-        });
-    }
-    notifyObservers() {
-        this.observers.forEach((observer) => {
-            observer.handleEvent.apply(observer, arguments);
-        });
-    }
-}
-
-class Observer {
-    handleEvent() {
-    }
-}
-
-module.exports.Observable = Observable;
-module.exports.Observer = Observer;
+module.exports.Observable = require('./lib/Observable.js');
+module.exports.ObservableSet = require('./lib/ObservableSet.js');
+module.exports.Observer = require('./lib/Observer.js');;
